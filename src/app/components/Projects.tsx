893ce866375231e95ef3f47998dc7e611e224e11
3akare <bakaredavid007@gmail.com>
2024-11-19 16:54:27 +0100

    feat: add projects section

---
+++ src/app/components/Projects.tsx
@@ -0,0 +1,11 @@
+export default function FeaturedWork() {
+    return (
+        <section className={"container mx-auto max-w-[90rem] w-full flex flex-wrap gap-6 md:gap-8 flex-col justify-center py-[3.75rem] px-[2.5rem] md:py-[5rem] lg:py-[7.5rem] md:px-[4rem]"}>
+            <h2 className={"text-xl font-semibold"}>
+               Projects
+            </h2>
+            <div>
+            </div>
+        </section>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/app/components/HeroSection";
+import FeaturedWork from "@/app/components/FeaturedWork";
 
 export default function Home() {
   return (
     <>
       <HeroSection/>
+        <FeaturedWork/>
     </>
   );
 }
