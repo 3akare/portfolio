72598553f2f7b4a8000f9604b060a1b943ccfa9a
3akare <bakaredavid007@gmail.com>
2024-12-01 15:11:36 +0100

    fix: corrected footer media-query error

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function Footer(){
     return(
         <footer
-            className={"container mx-auto max-w-[90rem] py-[80px] px-[40px] md:px-[64px] lg:py-[128px] lg:px-[80px] flex flex-row justify-between items-center capitalize"}>
-            <ul className={"flex-row gap-1 font-medium text-lg hidden md:flex"}>
+            className={"container mx-auto max-w-[90rem] py-[80px] px-[40px] md:px-[64px] lg:py-[128px] lg:px-[80px] flex flex-col gap-12 md:gap-0  md:flex-row justify-between items-center capitalize"}>
+            <ul className={"flex flex-row gap-4 font-medium text-lg "}>
                 <li className={"dark:text-[#B8B8B8] text-[#525252]"}>
                     <Link href={"./#work"}
                           className={"dark:hover:bg-[#212121] hover:bg-[#f7f7f7] py-[0.5rem] px-[1rem] transition-[background] duration-300 rounded-md"}>
@@ -24,7 +24,7 @@ export default function Footer(){
                     </Link>
                 </li>
             </ul>
-            <ul className={"flex-row gap-1 font-medium text-lg hidden md:flex"}>
+            <ul className={"flex flex-row gap-1 font-medium text-lg"}>
                 <li className={"dark:text-[#B8B8B8] text-[#525252]"}>
                     <Link href={"#"}
                           className={"dark:hover:bg-[#212121] hover:bg-[#f7f7f7] py-[0.5rem] px-[1rem] transition-[background] duration-300 rounded-md"}>
