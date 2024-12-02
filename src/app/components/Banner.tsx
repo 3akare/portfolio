08c8735f05f59f8273b787a16ea7867a697c858a
3akare <bakaredavid007@gmail.com>
2024-12-02 21:09:13 +0100

    fix: use opacity for banner link hover

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -13,7 +13,7 @@ export default function Banner() {
                     </h2>
                     <Link
                         href="mailto:bakaredavid007@gmail.com"
-                        className="text-2xl lg:text-3xl text-center font-semibold hover:text-[#999999] transition-colors duration-300"
+                        className="text-2xl lg:text-3xl text-center font-semibold hover:opacity-65 transition-[opacity] duration-300"
                     >
                         → bakaredavid007@gmail.com
                     </Link>
@@ -23,13 +23,13 @@ export default function Banner() {
             {/* Mobile Section */}
             <section className="py-[60px] block md:hidden">
                 <div
-                    className="flex flex-col items-center justify-center gap-12 px-[40px] py-[60px] bg-[linear-gradient(125deg,#fff5e6,#ffe6eb)] dark:bg-[linear-gradient(125deg,#2c0838,#901d4d,#ff6f66)]  text-center bg-[linear-gradient(121deg,#000000,15%,#901d4d,50%,#ff6f66)]">
+                    className="flex flex-col items-center justify-center gap-12 px-[40px] py-[60px] bg-[linear-gradient(125deg,#fff5e6,#ffe6eb)] dark:bg-[linear-gradient(125deg,#2c0838,#901d4d,#ff6f66)] text-center">
                     <h2 className="max-w-[700px] text-4xl font-semibold">
                         Want to create something <br/> awesome? Drop me an email.
                     </h2>
                     <Link
                         href="mailto:bakaredavid007@gmail.com"
-                        className="text-lg sm:text-xl mf:text-2xl text-center font-semibold hover:text-[#999999] transition-colors duration-300"
+                        className="text-lg sm:text-xl mf:text-2xl text-center font-semibold hover:opacity-65 transition-[opacity] duration-300"
                     >
                         → bakaredavid007@gmail.com
                     </Link>
