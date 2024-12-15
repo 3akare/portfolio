d16e1eb34d0de1a7b23788e0e17b26ee50f91ef0
3akare <bakaredavid007@gmail.com>
2024-12-15 17:09:11 +0100

    fix: fixed projects overflow

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -17,11 +17,11 @@ export default function Projects() {
                 {projects.map(({data:{image, description, title, link}}, index) => {
                     return (
                         <div key={index}
-                             className="rounded-t-xl md:rounded-t-2xl relative bg-transparent hover:-translate-y-1 transition-transform duration-[500ms] max-h-[265px] md:max-h-[400px]">
+                             className="rounded-t-xl md:rounded-t-2xl relative bg-transparent hover:-translate-y-1 transition-transform duration-[500ms] max-h-[265px] md:max-h-[400px] overflow-clip">
                             <Link href={link}>
                                 <Image src={image} alt={description} className="w-full min-h-[165px] lg:min-h-[400px] h-full object-cover rounded-2xl" priority/>
                                 <div
-                                    className="p-4 relative w-full -top-[6rem] dark:bg-[rgb(26,26,26)] bg-[#f7f7f7] rounded-b-xl md:rounded-b-2xl">
+                                    className="p-4 relative w-full -top-[5.75rem] dark:bg-[rgb(26,26,26)] bg-[#f7f7f7] rounded-b-xl md:rounded-b-2xl">
                                     <h3 className="text-base md:text-lg font-semibold">{title}</h3>
                                     <p className="text-sm lg:text-base h-10 font-medium text-[#757575] dark:text-[#999999]">{description}</p>
                                 </div>
