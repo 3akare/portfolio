a47fc13076aa0baed3814d57d933ced75144b2fc
3akare <bakaredavid007@gmail.com>
2024-12-15 16:49:19 +0100

    feat: updated projects 5 - 8, images and descriptions

--- public/data/projects.ts
+++ public/data/projects.ts
@@ -3,6 +3,10 @@ import asl_image from "../asl.webp"
 import nobr_image from "../nobr.webp"
 import webhook from "../webhook-notification.webp"
 import allcast_image from "../allcast.webp"
+import microservice from "../microservices.webp"
+import compiler from "../compiler.gif"
+import monty from "../monty.webp"
+import paycraft from "../paycraft.webp"
 
 export const projects = [
     {
@@ -114,7 +118,7 @@ export const projects = [
         id: 4,
         name: "Paycraft",
         data: {
-            image: image,
+            image: paycraft,
             title: "Paycraft",
             description: "Payroll management system.",
             link: "/projects/4",
@@ -125,13 +129,13 @@ export const projects = [
                 "TypeScript",
                 "Kora API"
             ],
-            liveUrl: "https://nobr-delta.vercel.app",
-            sourceUrl: "https://github.com/3akare/nobr",
+            liveUrl: "Not Available",
+            sourceUrl: "https://github.com/PayCraft-NG",
             paragraphs: [
                 "Paycraft is a payroll application built during a hackathon, designed to manage payroll processing, transaction histories, and employee-employer relations. The application includes features like authentication, employee and employer management, company management, USSD integrations, and a robust payroll engine. It leverages Kora's API for seamless payment integration.",
-                "This project was developed using Java (Spring Boot) for the backend and React with TypeScript for the frontend. As part of the development team, I was responsible for implementing key functionalities, including user authentication, employee management modules, and the payroll engine. The application showcased our ability to design scalable systems within a tight timeframe, earning us a fourth-place finish in the hackathon."
             ],
             additional: [
+                "This project was developed using Java (Spring Boot) for the backend and React with TypeScript for the frontend. As part of the development team, I was responsible for implementing key functionalities, including user authentication, employee management modules, and the payroll engine. The application showcased our ability to design scalable systems within a tight timeframe, earning us a fourth-place finish in the hackathon.",
                 "Though the application is no longer live due to hosting costs, it serves as a great example of teamwork, technical execution, and leveraging APIs like Kora for payment solutions.",
                 "The project was an exciting challenge that highlighted the importance of collaboration and quick problem-solving in a competitive environment."
             ]
@@ -141,7 +145,7 @@ export const projects = [
         id: 5,
         name: "Monty Interpreter",
         data: {
-            image: image,
+            image: monty,
             title: "Monty Bytecode Interpreter",
             description: "An interpreter for Monty bytecode.",
             link: "/projects/5",
@@ -165,7 +169,7 @@ export const projects = [
         id: 6,
         name: "Spring Boot Microservices",
         data: {
-            image: image,
+            image: microservice,
             title: "Microservices with Spring Boot",
             description: "Microservices using Spring Boot and Spring Cloud.",
             link: "/projects/6",
@@ -181,9 +185,9 @@ export const projects = [
             sourceUrl: "https://github.com/3akare/microservices",
             paragraphs: [
                 "This project is a bank application designed to demonstrate microservices architecture. It consists of four main services: account, loan, card, and customer services. Each service is modular, allowing for scalability and ease of integration. The system uses an API Gateway with a Eureka server to handle service discovery and communication.",
-                "The project is an ongoing effort to build a scalable and robust system. Planned integrations include Kubernetes for container orchestration, Redis for caching, and rate limiting to enhance performance and security. The project provides a practical learning experience in designing, implementing, and scaling microservices using Spring Boot and Spring Cloud."
             ],
             additional: [
+                "The project is an ongoing effort to build a scalable and robust system. Planned integrations include Kubernetes for container orchestration, Redis for caching, and rate limiting to enhance performance and security. The project provides a practical learning experience in designing, implementing, and scaling microservices using Spring Boot and Spring Cloud.",
                 "This project was initiated to deepen my understanding of microservices architecture, including concepts like service discovery, fault tolerance, and inter-service communication.",
                 "While still in progress, the application represents my commitment to mastering scalable system design and integrating modern tools like Kubernetes and Redis."
             ]
@@ -193,7 +197,7 @@ export const projects = [
         id: 7,
         name: "Tiny BASIC Compiler",
         data: {
-            image: image,
+            image: compiler,
             title: "Tiny BASIC Compiler",
             description: "A compiler for a simplified BASIC programming language.",
             link: "/projects/7",
@@ -205,10 +209,10 @@ export const projects = [
             liveUrl: "Not Available",
             sourceUrl: "https://github.com/3akare/Basic-Compiler",
             paragraphs: [
-                "This application translates Tiny BASIC code into a dynamically typed language, specifically JavaScript. The compiler is divided into three key components: the Lexer, Parser, and Emitter. The Lexer ensures syntax validity, building an Abstract Syntax Tree (AST) to confirm structural correctness. The Parser tokenizes the input code by breaking it into manageable parts, while the Emitter generates equivalent JavaScript code based on the syntax tree.",
-                "The project was motivated by a desire to deepen my understanding of compiler design. Initially inspired by an online article that provided a guide to building a Tiny BASIC-to-C compiler using Python, I decided to adapt the idea to fit my needs. At the time, I was learning Java and wanted to apply it to a practical project, avoiding the need to set up a C compiler. By targeting JavaScript, I avoided complexities associated with statically typed languages while sidestepping Python's strict indentation rules."
+                "This application translates Tiny BASIC code into JavaScript, a dynamically typed language. The compiler consists of three key components: the Lexer, Parser, and Emitter. The Lexer breaks the input code into tokens, ensuring it is syntactically valid. The Parser uses these tokens to build an Abstract Syntax Tree (AST), representing the structural correctness of the code. Finally, the Emitter generates equivalent JavaScript code based on the AST."
             ],
             additional: [
+                "The project was motivated by a desire to deepen my understanding of compiler design. Initially inspired by an online article that provided a guide to building a Tiny BASIC-to-C compiler using Python, I decided to adapt the idea to fit my needs. At the time, I was learning Java and wanted to apply it to a practical project, avoiding the need to set up a C compiler. By targeting JavaScript, I avoided complexities associated with statically typed languages while sidestepping Python's strict indentation rules.",
                 "The application is not live as there is no straightforward way to deploy a compiler, but the source code is fully available for use and adaptation.",
                 "The decision to target JavaScript as the output language was influenced by its flexibility as a dynamically typed language, making the compilation process simpler while still producing functional output. This project allowed me to enhance my skills in Java and compiler design, building a deeper understanding of tokenization, syntax validation, and code emission."
             ]
